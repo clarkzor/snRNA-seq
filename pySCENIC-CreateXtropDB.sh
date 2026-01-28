f965b94ff85139a4368d927e1c5103c445697a87
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 18:32:15 -0800

    Update and rename CreateXtropDB.sh to pySCENIC-CreateXtropDB.sh

--- pySCENIC-CreateXtropDB.sh
+++ pySCENIC-CreateXtropDB.sh
@@ -12,5 +12,4 @@ CBmotifDB=/dfs10/bio/clarklh/Rcistarget/create_cisTarget_databases/CBDB/
 CBmotifList=/dfs10/bio/clarklh/Rcistarget/create_cisTarget_databases/St105IraTotalTF_MotifList.csv
 OUTPUT=/dfs10/bio/clarklh/Rcistarget/create_cisTarget_databases/Output/ATAC_St12/ATAC_St12_50kbGeneRegions
 
-
 python create_cistarget_motif_databases.py -f $FASTA -M $CBmotifDB -m $CBmotifList -o $OUTPUT -t 35 
