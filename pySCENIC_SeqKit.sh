6af6be6fb207e69e8463e52aa322be462b704b0d
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 18:35:26 -0800

    Update and rename SeqKit.sh to pySCENIC_SeqKit.sh

--- pySCENIC_SeqKit.sh
+++ pySCENIC_SeqKit.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=20 ##number of cores to use per node
 #SBATCH --error=slurm-%J.err ##Error file
 
-UPSTREAM=Xtrop_AllGenes_20kbUpstreamRegionsSt105.fasta
-DOWNSTREAM=Xtrop_AllGenes_20kbDownstreamRegionsSt105.fasta
-OUTPUT=Xtrop_TotalGTFmRNA_Up-Downstream_20kb_GeneRegions.fasta
+UPSTREAM=Xtrop_AllGenes_5kbUpstreamRegionsSt105.fasta
+DOWNSTREAM=Xtrop_AllGenes_5kbDownstreamRegionsSt105.fasta
+OUTPUT=Xtrop_TotalGTFmRNA_Up-Downstream_5kb_GeneRegions.fasta
 
 seqkit concat $UPSTREAM $DOWNSTREAM -o $OUTPUT
