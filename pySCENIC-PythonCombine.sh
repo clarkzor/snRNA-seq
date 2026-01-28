8b3a7680508417bac0921d1a4f00dc0c3d7d403c
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-01-27 18:35:55 -0800

    Update and rename PythonCombine.sh to pySCENIC-PythonCombine.sh

--- pySCENIC-PythonCombine.sh
+++ pySCENIC-PythonCombine.sh
@@ -6,11 +6,7 @@
 #SBATCH --cpus-per-task=10 ##number of cores to use per node
 #SBATCH -error=slurm-%J.err ##Error file
 
-
 INPUT=/dfs10/bio/clarklh/ATAC/St12/MACS2/ATAC_St12_closestBed_50kb_genes_ForFasta.fasta
 OUTPUT=/dfs10/bio/clarklh/ATAC/St12/MACS2/ATAC_St12_closestBed_50kb_genes_ForFasta-PythonCombine.fasta
 
 python SeqMerge.py $INPUT > $OUTPUT
-
-
-
