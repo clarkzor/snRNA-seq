f1490f4a7d59567a688314263b6fd51b278b3f62
Clark Hendrickson <32344594+clarkzor@users.noreply.github.com>
2026-07-27 17:25:02 -0700

    Delete QC and Annotation Analysis/scripts directory

--- QC and Annotation Analysis/scripts/differential_expression.py
+++
@@ -1,6 +0,0 @@
-def run_rank_genes_groups(adata, groupby, method="t-test"):
-    sc.tl.rank_genes_groups(adata, groupby=groupby, method=method)
-    return adata
-
-def export_rank_genes_groups(adata, output_path):
-    ...
